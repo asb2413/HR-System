95804b4815ccb9d57ab89f8ccf75fd65ab110c78
Abdullahs <abdullahs2413@gmail.com>
2023-06-10 20:23:47 +0300

    json changes

--- frontend/src/components/Sections_Emp.js
+++ frontend/src/components/Sections_Emp.js
@@ -17,9 +17,7 @@ function Sections() {
             </div>
             
             <span className="text">
-              From this page, you can register new employees 
-               in the system.
-               Only HR accounts can use this page.
+            The employee portal provides access only to the employee center, where you can view all your personal information such as payroll, attendance, and requests. Additionally, you can submit requests and await approval, and track the status of your requests along with the name of the approver.
             </span>
           </div>
           <Link to={'/emp/empdetails'}>
