589e40781ebbe910d7b14e1789c036161c7b611f
unknown <abdullahs2413@gmail.com>
2024-06-12 06:10:23 +0300

    a

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,20 @@ const server = express();
 const cors = require('cors')
 require("dotenv").config();
 const mongoose = require("mongoose");
-const port = process.env.PORT ;
+const port = process.env.PORT || 4000;
 mongoose.set("strictQuery", false);
 const userRouter = require("./routes/HR routes/user");
 const homeRouter = require("./routes/HR routes/home");
 const empUserRouter = require("./routes/EMP routes/user");
 const EmphomeRouter = require("./routes/EMP routes/home");
 server.use(express.json());
-const corsOptions = {
-  origin: 'https://ornate-paprenjak-c573a0.netlify.app', // specify your frontend's origin
-  credentials: true, // allow credentials (cookies, authorization headers, etc.)
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  preflightContinue: false,
-  optionsSuccessStatus: 204 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+server.use(cors({
+  
+  origin:"*",
+  credentials:true,
 
-app.use(cors(corsOptions));
+}))
 
-// Preflight requests handling
-app.options('*', cors(corsOptions));
 //hr routes
 server.use("/hr", userRouter);
 server.use("/hr", homeRouter);
@@ -36,7 +30,7 @@ mongoose
   .then(() => {
     console.log("db is connected");
     //listen
-    server.listen(process.env.PORT , () => {
+    server.listen(process.env.PORT, () => {
       console.log("listening to prot ", process.env.PORT);
     });
   })
