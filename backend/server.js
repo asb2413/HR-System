b3f35e845bc4529f9024d4276438c3a05ebe8173
unknown <abdullahs2413@gmail.com>
2024-06-12 05:52:15 +0300

    a

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const server = express();
 const cors = require('cors')
 require("dotenv").config();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 4000;
+
 mongoose.set("strictQuery", false);
 const userRouter = require("./routes/HR routes/user");
 const homeRouter = require("./routes/HR routes/home");
@@ -36,7 +36,7 @@ mongoose
   .then(() => {
     console.log("db is connected");
     //listen
-    server.listen(process.env.PORT , () => {
+    server.listen(process.env.PORT || 4000 , () => {
       console.log("listening to prot ", process.env.PORT);
     });
   })
